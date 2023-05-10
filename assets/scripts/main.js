b981376954606ee8d2fa29302029ef937cc95c5b
adarsh249 <adarshpatel249@gmail.com>
2023-05-10 11:46:18 -0700

    Saving progress for today

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -24,6 +24,10 @@ function getRecipesFromStorage() {
   // A9. TODO - Complete the functionality as described in this function
   //           header. It is possible in only a single line, but should
   //           be no more than a few lines.
+  if(localStorage.getItem("recipes") === null)
+    return new Array();
+  else
+    return localStorage.getItem("recipes");
 }
 
 /**
@@ -35,10 +39,18 @@ function getRecipesFromStorage() {
  */
 function addRecipesToDocument(recipes) {
   // A10. TODO - Get a reference to the <main> element
+  let main = document.querySelector("main");
   // A11. TODO - Loop through each of the recipes in the passed in array,
   //            create a <recipe-card> element for each one, and populate
   //            each <recipe-card> with that recipe data using element.data = ...
   //            Append each element to <main>
+  console.log(recipes.length);
+  for(let i = 0; i < recipes.length; i++){
+    let newRecipeCard = document.createElement("recipe-card");
+    newRecipeCard.data = recipes[i].imgSrc;
+    console.log(newRecipeCard);
+    main.append(newRecipeCard);
+  }
 }
 
 /**
